ae72bd527e9c46748c13c46e4f862cd5a8f5c357
Kachitaro <anhtai741981@gmail.com>
2022-12-14 18:51:50 +0700

    delete log

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -58,7 +58,6 @@ let handleEditUser = async (req, res) => {
 };
 
 let handleDeleteUser = async (req, res) => {
-    console.log(req.body.id);
     if (!req.body.id) {
         return res.status(200).json({
             errCode: 1,
